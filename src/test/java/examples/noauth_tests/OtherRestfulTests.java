67799d2758908f4c513933bcb782e33572cb58fa
Aplnaa <aleksandrovv_a@mail.ru>
2025-04-06 13:46:31 +0700

    добавила получение после POST запроса id для последующих тестов на обновление/удаление данных

--- src/test/java/examples/noauth_tests/OtherRestfulTests.java
+++ src/test/java/examples/noauth_tests/OtherRestfulTests.java
@@ -3,19 +3,21 @@ package examples.noauth_tests;
 import io.restassured.response.Response;
 import jdk.jfr.Description;
 import methods.rest_steps.Steps;
+import org.junit.jupiter.api.*;
 import requests.noauth.RequestPatchRestful;
 import requests.noauth.RequestPostRestful;
 import requests.RequestPutRestful;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 
 /*
     Автотесты без авторизации по методам: POST, PUT, PATCH, DELETE
 */
 @DisplayName("POST, PUT, PATCH and DELETE tests")
+@TestMethodOrder(OrderAnnotation.class)
 public class OtherRestfulTests {
     private static String BASE_URL = "https://api.restful-api.dev";
+    private static String createdId;
     private String endpoint;
     private Steps steps;
 
@@ -25,6 +27,7 @@ public class OtherRestfulTests {
     }
 
     @Test
+    @Order(1)
     @DisplayName("POST: /objects")
     @Description("Создание объекта")
     public void sendPostRequest(){
@@ -32,33 +35,38 @@ public class OtherRestfulTests {
         RequestPostRestful request = new RequestPostRestful("Apple MacBook Pro 16", new RequestPostRestful.Data(2019, 1849.99, "Intel Core i9", "1 TB"));
         Response response = steps.sendPostRequest(endpoint, request);
         steps.checkStatusCode(response, 200);
+
+        createdId = response.jsonPath().getString("id");
     }
 
     @Test
+    @Order(2)
     @DisplayName("PUT: /objects/{id}")
     @Description("Обновление всего объекта по айди")
     public void testPut(){
-        endpoint = "/objects/ff808181932badb60195f74dd2690615";
+        endpoint = "/objects/" + createdId;
         RequestPutRestful request = new RequestPutRestful("Apple MacBook Pro 16", new RequestPutRestful.Data(2019, 1849.99, "gray","Intel Core i9", "1 TB"));
         Response response = steps.sendPutRequest(endpoint, request);
         steps.checkStatusCode(response, 200);
     }
 
     @Test
+    @Order(3)
     @DisplayName("PATCH: /objects/{id}")
     @Description("Обновление конкретного параметра в объекте по айди")
     public void testPatch(){
-        endpoint = "/objects/ff808181932badb60195f74dd2690615";
+        endpoint = "/objects/" + createdId;
         RequestPatchRestful request = new RequestPatchRestful("Apple MacBook Air 13 2020");
         Response response = steps.sendPatchRequest(endpoint, request);
         steps.checkStatusCode(response, 200);
     }
 
     @Test
+    @Order(4)
     @DisplayName("DELETE: /objects/{id}")
     @Description("Удаление всего объекта по айди")
     public void testDelete(){
-        endpoint = "/objects/ff808181932badb60195f74dd2690615";
+        endpoint = "/objects/" + createdId;
         Response response = steps.sendDeleteRequest(endpoint);
         steps.checkStatusCode(response, 200);
     }
